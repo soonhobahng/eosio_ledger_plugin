43d1749deec6813dec8e0b7916b8ac49b56daeed
stephen bahng <stephen.bahng@gmail.com>
2018-12-06 13:50:27 +0900

    bug fixed

--- dbconn/dbconn.hpp
+++ dbconn/dbconn.hpp
@@ -11,14 +11,15 @@
 
 using std::shared_ptr;
 
+using namespace mysqlx;
+
 namespace eosio{
 class dbconn {
     public:
         explicit dbconn(const std::string host, const std::string user, const std::string passwd, const std::string database, const uint16_t port, const uint16_t max_conn);
         ~dbconn();
 
-        Session* get_connection();
-        void release_connection(Session* sess);
+        Session get_connection();
 
     private:
         Client *cli;
