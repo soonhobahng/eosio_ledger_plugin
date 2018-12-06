b7dbe13e4574b351d4e1dc6a7e76da6e60246e87
stephen bahng <stephen.bahng@gmail.com>
2018-12-06 16:21:25 +0900

    bug fixed

--- db/connection_pool.cpp
+++ db/connection_pool.cpp
@@ -12,7 +12,7 @@ namespace eosio {
 
     connection_pool::connection_pool( 
             const std::string host, const std::string user, const std::string passwd, const std::string database, 
-            const uint16_t port, const uint16_t max_conn, const bool do_closeconn_on_unlock):
+            const uint16_t port, const uint16_t max_conn):
     m_pool(max_conn,host,user,passwd,database,port), _do_closeconn_on_unlock(do_closeconn_on_unlock)
     {
         /*
--- db/connection_pool.h
+++ db/connection_pool.h
@@ -10,7 +10,7 @@ class connection_pool
     public:
         explicit connection_pool(
             const std::string host, const std::string user, const std::string passwd, const std::string database, 
-            const uint16_t port, const uint16_t max_conn, const bool do_closeconn_on_unlock);
+            const uint16_t port, const uint16_t max_conn);
         ~connection_pool();
         
         shared_ptr<MysqlConnection> get_connection();
