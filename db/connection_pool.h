a320b6c7daffb9d222522af118f76abfc7d697f1
stephen bahng <stephen.bahng@gmail.com>
2018-12-06 15:17:50 +0900

    change mysql module

---
+++ db/connection_pool.h
@@ -0,0 +1,26 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <memory>
+#include "mysqlconn.h"
+
+namespace eosio {
+class connection_pool
+{
+    public:
+        explicit connection_pool(
+            const std::string host, const std::string user, const std::string passwd, const std::string database, 
+            const uint16_t port, const uint16_t max_conn, const bool do_closeconn_on_unlock);
+        ~connection_pool();
+        
+        shared_ptr<MysqlConnection> get_connection();
+        void release_connection(MysqlConnection& con);
+
+    private:
+        MysqlConnPool m_pool;
+
+        bool    _do_closeconn_on_unlock;
+};
+}
+
+#endif
