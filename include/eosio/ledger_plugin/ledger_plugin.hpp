7bb79477be2b26f3a2c41f418632c362f86f4c36
stephen bahng <stephen.bahng@gmail.com>
2018-12-06 16:44:40 +0900

    bug fixed

--- include/eosio/ledger_plugin/ledger_plugin.hpp
+++ include/eosio/ledger_plugin/ledger_plugin.hpp
@@ -9,17 +9,16 @@
 
 namespace eosio {
 
-using namespace appbase;
-
 /**
  *  This is a template plugin, intended to serve as a starting point for making new plugins
  */
-class ledger_plugin : public appbase::plugin<ledger_plugin> {
+class ledger_plugin : public plugin<ledger_plugin> {
 public:
+   APPBASE_PLUGIN_REQUIRES((chain_plugin))
+   
    ledger_plugin();
    virtual ~ledger_plugin();
- 
-   APPBASE_PLUGIN_REQUIRES((chain_plugin))
+
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
