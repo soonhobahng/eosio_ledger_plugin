b4055c5569b728b31dc5d2c79f440eea5ebae55f
stephen bahng <stephen.bahng@gmail.com>
2018-12-12 18:31:28 +0900

    debug

--- ledger_plugin.cpp
+++ ledger_plugin.cpp
@@ -259,7 +259,7 @@ void ledger_plugin_impl::process_add_ledger( const chain::action_trace& atrace )
    const auto block_time = atrace.block_time;
    
    if(atrace.act.name == N(transfer) || atrace.act.name == N(create)) {
-      // ilog("action_id : ${a}",("a",action_id));
+      ilog("action_id : ${a}",("a",action_id));
       m_ledger_table->add_ledger(action_id, trx_id, block_number, block_time, atrace.receipt.receiver.to_string(), atrace.act);
    }
       
