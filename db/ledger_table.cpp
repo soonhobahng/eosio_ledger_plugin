90e3c22835bf236b13b62b53d44e73be12d00552
stephen bahng <stephen.bahng@gmail.com>
2018-12-12 17:25:45 +0900

    bug fixed

--- db/ledger_table.cpp
+++ db/ledger_table.cpp
@@ -282,7 +282,7 @@ void ledger_table::tick(const int64_t tick) {
 }
 
 void ledger_table::post_raw_query() {
-    const std::string query_str = raw_bulk_sql.str();
+    std::string query_str = raw_bulk_sql.str();
     query_str.pop_back();
 
     if (raw_bulk_count) {
